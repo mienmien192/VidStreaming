8eb8c6c716cf7a6699f5cf794e074c71d08c1e85
nguyenxuanda0550 <18520550@gm.uit.edu.vn>
2021-11-02 07:45:19 +0700

    make it better

--- assets/js/rtc.js
+++ assets/js/rtc.js
@@ -201,7 +201,7 @@ window.addEventListener( 'load', () => {
                     let controlDiv = document.createElement( 'div' );
                     controlDiv.className = 'remote-video-controls';
                     controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
-                        <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i><p style="color: red;">${username}(${randomNumber})</p>`;
+                        <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i><p style="color: red;">${username}</p>`;
 
                     //create a new div for card
                     let cardDiv = document.createElement( 'div' );
